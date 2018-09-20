9e0a6f6b45adf83233da4ed225f0c00207b91cc5
Rubens S. Takauti <rtakauti@hotmail.com>
2018-09-20 13:33:50 -0300

    Simplifica

--- src/Atm.php
+++ src/Atm.php
@@ -4,14 +4,12 @@ namespace Dojo;
 
 class Atm
 {
-    private $availableBills;
     private $billCount;
     private $index = 0;
 
     public function __construct(...$bills)
     {
-        $this->availableBills = (array)$bills;
-        $this->billCount = array_fill_keys($this->availableBills, 0);
+        $this->billCount = array_fill_keys((array)$bills, 0);
     }
 
     public function withdraw($withdraw)
@@ -21,13 +19,13 @@ class Atm
         }
 
         while ($withdraw > 0) {
-            if (($withdraw - $this->availableBills[$this->index]) < 0) {
+            if (($withdraw - array_keys($this->billCount)[$this->index]) < 0) {
                 $this->index++;
                 continue;
             }
 
-            $withdraw -= $this->availableBills[$this->index];
-            ++$this->billCount[$this->availableBills[$this->index]];
+            $withdraw -= array_keys($this->billCount)[$this->index];
+            ++$this->billCount[$this->index];
         }
 
         return array_filter($this->billCount);
